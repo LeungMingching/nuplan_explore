f9ad1c62613f1300e47b5d2d33bf6773f2d7b1cf
Liang Mingjing <liangmingjing849@163.com>
2023-03-16 14:37:37 +0800

    done conversion by json
    Signed-off-by: Liang Mingjing <liangmingjing849@163.com>

--- conversion/convert_dataset_by_json.py
+++ conversion/convert_dataset_by_json.py
@@ -20,6 +20,13 @@ def discover_labels(index_dir):
     sub_folders = [name for name in os.listdir(index_dir) if os.path.isdir(os.path.join(index_dir, name))]
     return sub_folders
 
+def align_length(array1, array2):
+    if len(array1) > len(array2):
+        array1 = array1[:len(array2)]
+    elif len(array1) < len(array2):
+        array2 = array2[:len(array1)]
+    return array1, array2
+
 def resize_num_agents(agents_state_array, num_objcts, num_features):
     resized_agents_state_array = agents_state_array.copy()
     resized_agents_state_array.resize((num_objcts,num_features))
@@ -128,6 +135,99 @@ def construct_agents_state_by_timestamp(
 
     return agents_state_array
 
+def get_observation_timestamp_range(
+    log_db_file,
+    clip_start_timestamp,
+    clip_end_timestamp,
+    past_time_horizon,
+    future_time_horizon
+):
+    db_timestamp_array = np.array(get_all_timestamps_from_db(log_db_file))
+    db_timestamp_array.sort()
+    idx_obs_start = find_nearest_idx(
+        db_timestamp_array, clip_start_timestamp + past_time_horizon*1e6)
+    idx_obs_end = find_nearest_idx(
+        db_timestamp_array, clip_end_timestamp - future_time_horizon*1e6)
+    return db_timestamp_array[idx_obs_start:idx_obs_end]
+
+def get_look_ahead_pt_timestamp_range(
+    log_db_file,
+    clip_start_timestamp,
+    clip_end_timestamp,
+    past_time_horizon,
+    future_time_horizon
+):
+    db_timestamp_array = np.array(get_all_timestamps_from_db(log_db_file))
+    db_timestamp_array.sort()
+    idx_lap_start = find_nearest_idx(
+        db_timestamp_array, clip_start_timestamp + past_time_horizon*1e6 + future_time_horizon*1e6)
+    idx_lap_end = find_nearest_idx(
+        db_timestamp_array, clip_end_timestamp)
+    return db_timestamp_array[idx_lap_start:idx_lap_end]
+
+def build_observation_between_timestamps(
+    log_db_file,
+    timestamp_array,
+    roi_radius,
+    num_objects
+):
+    observation_list = []
+    # loop all frames within a clip
+    for idx in range(1, len(timestamp_array)-1):
+        timestamp = timestamp_array[idx]
+        prev_timestamp = timestamp_array[idx - 1]
+        future_timestamp = timestamp_array[idx + 1]
+
+        ego_state_array = construct_ego_state_by_timestamp(log_db_file, timestamp)
+        agents_state_array = construct_agents_state_by_timestamp(log_db_file, timestamp, prev_timestamp, future_timestamp, ego_state_array, roi_radius, num_objects)
+        ego_state_array = np.expand_dims(ego_state_array, axis=0)
+        observation = np.concatenate((ego_state_array, agents_state_array), axis=0)
+        observation_list.append(observation)
+    return np.asarray(observation_list)
+
+def build_look_ahead_pt_between_timestamps(
+    log_db_file,
+    timestamp_array
+):
+    look_ahead_pt_list = []
+    for idx in range(1, len(timestamp_array)-1):
+        timestamp = timestamp_array[idx]
+        ego_state_array = construct_ego_state_by_timestamp(log_db_file, timestamp)
+        look_ahead_pt_x = ego_state_array[0]
+        look_ahead_pt_y = ego_state_array[1]
+        look_ahead_pt_v = np.linalg.norm(
+            np.asarray([ego_state_array[3], ego_state_array[4]]))
+        look_ahead_pt = np.array([look_ahead_pt_x, look_ahead_pt_y, look_ahead_pt_v])
+        look_ahead_pt_list.append(look_ahead_pt)
+    return np.asarray(look_ahead_pt_list)
+
+def extract_data_by_json(
+    log_db_file,
+    clip_start_timestamp,
+    clip_end_timestamp,
+    past_time_horizon,
+    future_time_horizon,
+    roi_radius,
+    num_objects
+):
+    # observation
+    observation_timestamp_range = get_observation_timestamp_range(
+        log_db_file, clip_start_timestamp, clip_end_timestamp, past_time_horizon, future_time_horizon)
+    observation_array = build_observation_between_timestamps(
+        log_db_file, observation_timestamp_range, roi_radius, num_objects)
+    
+    # loop-ahead point
+    look_ahead_pt_timestamp_range = get_look_ahead_pt_timestamp_range(
+        log_db_file, clip_start_timestamp, clip_end_timestamp, past_time_horizon, future_time_horizon)
+    look_ahead_pt_array = build_look_ahead_pt_between_timestamps(
+        log_db_file, look_ahead_pt_timestamp_range)
+    
+    # align
+    observation_array, look_ahead_pt_array = align_length(observation_array, look_ahead_pt_array)
+    assert len(observation_array) == len(look_ahead_pt_array), 'Not equal length'
+    
+    return observation_array, look_ahead_pt_array
+
 def main(config):
     # read config
     db_root = config['db_root']
@@ -138,6 +238,8 @@ def main(config):
     roi_radius = config['roi_radius']
     num_objects = config['num_objects']
     
+    toc = {}
+    toc['total_frame'] = 0
     # loop for all labels
     label_list = discover_labels(json_root)
     for label in label_list:
@@ -147,45 +249,41 @@ def main(config):
         
         # loop for all json files
         index_file_list = glob(os.path.join(json_root, label, '*'))
-        for idx_file in index_file_list:
+        toc[label] = {}
+        toc[label]['clip'] = len(index_file_list)
+        toc[label]['frame'] = 0
+        for idx_file in tqdm(index_file_list, desc=f'Working on {label}'):
             with open(idx_file, 'rb') as f:
                 index_dict = json.load(f)
             log_db_file = os.path.join(db_root, index_dict['log_db_file'])
-            start_timestamp = index_dict['clip_head']['timestamp']
-            end_timestamp = index_dict['clip_tail']['timestamp']
+            clip_start_timestamp = index_dict['clip_head']['timestamp']
+            clip_end_timestamp = index_dict['clip_tail']['timestamp']
 
-            # TODO: get_observation_timestamp_range()
-            db_timestamp_array = np.array(get_all_timestamps_from_db(log_db_file))
-            db_timestamp_array.sort()
-            idx_obs_start = find_nearest_idx(db_timestamp_array, start_timestamp + past_time_horizon*1e6) + 1 # +1 to calculate acceleration
-            idx_obs_end = find_nearest_idx(db_timestamp_array, end_timestamp - future_time_horizon*1e6)
-            # TODO: build_observation_between_timestamps()
-            observation_list = []
-            # loop all frames within a clip
-            for idx in range(idx_obs_start, idx_obs_end):
-                timestamp = db_timestamp_array[idx]
-                prev_timestamp = db_timestamp_array[idx - 1]
-                future_timestamp = db_timestamp_array[idx + 1]
-
-                ego_state_array = construct_ego_state_by_timestamp(log_db_file, timestamp)
-                agents_state_array = construct_agents_state_by_timestamp(log_db_file, timestamp, prev_timestamp, future_timestamp, ego_state_array, roi_radius, num_objects)
-                ego_state_array = np.expand_dims(ego_state_array, axis=0)
-                observation = np.concatenate((ego_state_array, agents_state_array), axis=0)
-                observation_list.append(observation)
-            observation_list = np.asarray(observation_list)
+            # skip if converted
+            observation_file = os.path.join(label_path, f'{clip_start_timestamp}_observation.npy')
+            look_ahead_pt_file = os.path.join(label_path, f'{clip_start_timestamp}_look_ahead_pt.npy')
+            if os.path.exists(look_ahead_pt_file):
+                continue
             
-            # TODO: get_look_ahead_pt_timestamp_range()
-            # TODO: build_observation_between_timestamps()
-            # look ahead point
-            look_ahead_pt_list = []
+            # extract
+            observation_array, look_ahead_pt_array = extract_data_by_json(
+                log_db_file, clip_start_timestamp, clip_end_timestamp,
+                past_time_horizon, future_time_horizon, roi_radius, num_objects)
 
-            
-            print(f'log_db_file: \n{log_db_file}')
-            print(f'start_timestamp: \n{start_timestamp}')
-            print(f'end_timestamp: \n{end_timestamp}')
-            print(f'db_timestamp_array: \n{db_timestamp_array}')
-            print(f'observation_list len: \n{len(observation_list)}')
+            toc[label]['frame'] += len(look_ahead_pt_array)
+            toc['total_frame'] += len(look_ahead_pt_array)
+            # save
+            with open(observation_file, 'wb') as f:
+                np.save(f, observation_array)
+            with open(look_ahead_pt_file, 'wb') as f:
+                np.save(f, look_ahead_pt_array)
 
+    toc_path = os.path.join(coverted_root, 'table_of_content.json')
+    if not os.path.exists(toc_path):
+        json_f = json.dumps(toc, indent=4, sort_keys=True)
+        with open(toc_path, 'w') as f:        
+            f.write(json_f)
+    print('Done!')
     pass
 
 if __name__ == '__main__':
